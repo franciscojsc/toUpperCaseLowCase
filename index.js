24de8b99e5085e0d674141707d5f7ebbe6510a90
Francisco Chaves <francisco.jschaves@gmail.com>
2019-05-25 17:37:55 -0300

    Add project

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+	upperCase,
+	lowerCase
+};
+
+function upperCase(letters) {
+	return letters.toUpperCase();
+}
+
+function lowerCase(letters) {
+	return letters.toLowerCase();
+}
